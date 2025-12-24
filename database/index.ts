f9d811c788a39354dea4dc74c366eaf40ed5568a
Cephas30 <cephasarhin@gmail.com>
2025-12-24 23:55:39 +0000

    Implement database models

---
+++ database/index.ts
@@ -0,0 +1,5 @@
+export { Event } from './event.model';
+export type { EventDocument, EventMode } from './event.model';
+
+export { Booking } from './booking.model';
+export type { BookingDocument } from './booking.model';
