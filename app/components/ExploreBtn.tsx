82c4ef3ccc55b20c6ed9ef4ae44db62dda0f7d9b
Cephas30 <cephasarhin@gmail.com>
2025-12-24 13:45:51 +0000

    first commit

---
+++ app/components/ExploreBtn.tsx
@@ -0,0 +1,32 @@
+'use client'
+import Image from 'next/image'
+import React from 'react'
+
+const ExploreBtn = () => {
+  return (
+    <button
+      type="button"
+      onClick={() => {
+        console.log("Explore button clicked")
+        document.getElementById('events')?.scrollIntoView({ behavior: 'smooth' })
+      }}
+      className="
+        mt-7 mx-auto flex items-center gap-2
+        bg-orange-600 text-white
+        px-6 py-3 rounded-full
+        hover:bg-orange-700 transition
+      "
+    >
+      Explore Events
+      <Image
+        src="/icons/arrow-down.svg"
+        alt="arrow-down"
+        width={24}
+        height={24}
+      />
+    </button>
+    
+  )
+}
+
+export default ExploreBtn
---
+++ lib/constants.ts
@@ -0,0 +1,50 @@
+export const events = [
+  {
+    title: "CES 2026",
+    image: "/images/event1.png",
+    slug: "ces-2026",
+    location: "Las Vegas, NV",
+    date: "2026-01-07",
+    time: "10:00 AM"
+  },
+  {
+    title: "Google I/O 2026",
+    image: "/images/event2.png",
+    slug: "google-io-2026",
+    location: "Mountain View, CA",
+    date: "2026-05-14",
+    time: "9:00 AM"
+  },
+  {
+    title: "WWDC 2026",
+    image: "/images/event3.png",
+    slug: "wwdc-2026",
+    location: "Cupertino, CA",
+    date: "2026-06-08",
+    time: "10:00 AM"
+  },
+  {
+    title: "React Conf 2026",
+    image: "/images/event4.png",
+    slug: "react-conf-2026",
+    location: "Henderson, NV",
+    date: "2026-05-15",
+    time: "9:00 AM"
+  },
+  {
+    title: "F8 2026",
+    image: "/images/event5.png",
+    slug: "f8-2026",
+    location: "San Francisco, CA",
+    date: "2026-05-06",
+    time: "10:00 AM"
+  },
+  {
+    title: "Major League Hacking Hackathon",
+    image: "/images/event6.png",
+    slug: "mlh-hackathon-2026",
+    location: "Virtual",
+    date: "2026-03-15",
+    time: "12:00 PM"
+  }
+];
